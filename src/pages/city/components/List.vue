d41f478323b0946fa4cabc0292cf024aa0960e88
hanhailin <hanhailin@jianke.com>
2019-04-10 17:48:05 +0800

    touch_start_and_touch_move

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -21,7 +21,7 @@
             </div>
         </div>
     </div>
-    <div class="area" v-for="(item,key) of cities" :key="key">
+    <div class="area" v-for="(item,key) of cities" :key="key" :ref="key">
         <div class="title border-topbottom">
             {{key}}
         </div>
@@ -43,10 +43,20 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      console.log(this.letter)
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
